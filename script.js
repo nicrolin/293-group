469398e7d9fe62a6967eafe0727037da29b0f162
nicrolin <laaldlalala@gmail.com>
2026-03-08 17:38:55 +0500

    Add Russian messages and adjust button font size

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ты уверена? 😊",
+    "Может всё-таки пойдешь? ☕",
+    "Будет вкусный кофе ☕",
+    "И десерт тоже 😋",
+    "8 марта всё таки 🌸",
+    "Я очень стараюсь 😅",
+    "Ну пожалуйста 🌸",
+    "Обещаю хорошее настроение",
+    "Последний шанс 😄",
+    "Скажи да ☕🌸"
 ];
 
 let messageIndex = 0;
@@ -16,12 +16,34 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
+
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
+
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 1.2}px`;
 }
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
 }
+
+
+// 🌸 падающие цветки
+function createFlower() {
+    const flower = document.createElement("div");
+    flower.classList.add("flower");
+
+    flower.style.left = Math.random() * window.innerWidth + "px";
+    flower.style.animationDuration = (Math.random() * 3 + 3) + "s";
+
+    flower.innerHTML = "🌸";
+
+    document.body.appendChild(flower);
+
+    setTimeout(() => {
+        flower.remove();
+    }, 6000);
+}
+
+setInterval(createFlower, 300);
